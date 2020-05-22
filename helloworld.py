1bdb9a2c0cd606c4f801427a2991f7d35667420c
dakotachasesmith <dakotachasesmith@gmail.com>
2020-05-22 05:59:27 -1000

    Create a task to rotate the camera in a circle

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,8 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+
 
 
 class MyApp(ShowBase):
@@ -13,6 +17,16 @@ class MyApp(ShowBase):
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
+
+        # Add the spinCameraTask procedure to the task manager.
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
 
 app = MyApp()
 app.run()
